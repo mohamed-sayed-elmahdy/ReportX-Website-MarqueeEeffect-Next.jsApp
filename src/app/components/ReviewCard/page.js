b59d608491f1eecc0dbbfc1261feaa9e31d82b24
Mohamed Sayed <ms7237322@gmail.com>
2024-05-11 18:45:26 +0300

    first commit

--- src/app/components/ReviewCard/page.js
+++ src/app/components/ReviewCard/page.js
@@ -6,7 +6,7 @@ function page() {
   return (
     <div className={styles.cardContainer}>
       <Image
-        src="/apple.png"
+        src="/pic.png"
         alt="stars"
         width={110}
         height={15}
