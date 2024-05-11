63edfbd2d8640b0c27874cf3f9e273c7a70b9e5c
Mohamed Sayed <ms7237322@gmail.com>
2024-05-11 18:50:28 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
         <div className={`${styles.reviewsContainer} flex`}>
           <span>4.6</span>
           <Image
-            src="/Group 3.png"
+            src="/groupstars.png"
             alt="stars"
             width={110}
             height={20}
